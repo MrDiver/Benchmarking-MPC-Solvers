ed47ff94c29a3b04ed698d0a729ac3e3894fda0d
mewmiyu <xiaoran.miyu@gmail.com>
2021-02-16 22:57:54 +0100

    kalman filter works on the example problem)

--- MPCBenchmark/i2c/kalman.py
+++ MPCBenchmark/i2c/kalman.py
@@ -3,35 +3,45 @@ import numpy as np
 
 class kalman:
 
-    def __init__(self, A, E, C, y_t, mu_t, var_t) -> None:
+    def __init__(self, A, a, B, eta, zeta, C, y_t, mu_t, var_t) -> None:
         super().__init__()
         self.A = A
-        self.E = E
+        self.a = a
+        self.B = B
+        self.eta = eta
         self.C = C
         self.y_t = y_t
+        self.zeta = zeta
 
         self.mu_t = mu_t
         self.var_t = var_t
 
-    def calc_filter(self):
-        self.predict_step(self.A, self.E)
-        self.update_step(self.C, self.E, self.y_t)
+    def dynamics(self, x, u):
+        return self.A @ x + self.a + self.B @ u
 
-    def predict_step(self, A, E):
+    def calc_filter(self, u, y):
+        self.predict_step(self.A, self.eta, u)
+        self.update_step(self.C, self.zeta, y)
+        # return the mean of state and the mean of observation
+        # e.g. voltage = current*resistance -> mu is current and c*mu is voltage
+        return self.mu_x[:, 0], (self.C @ self.mu_x)[:, 0]
+
+    def predict_step(self, A, eta, u):
         """
         A = transition matrix
-        E = process noise covariance matrix
+        eta = process noise covariance matrix
         """
-        self.mu_t = A @ self.mu_t
-        self.var_t = A @ self.var_t @ A.T + E
+        self.mu_x = self.dynamics(self.mu_x, u)
+        self.sig_x = A @ self.sig_x @ A.T + eta
 
-    def update_step(self, C, E, y_t):
+    def update_step(self, C, zeta, y_t):
         """
-        L_t =
-        C = cost matrix
-        E = process noise covariance matrix
+        L_t = Kalman gain matrix
+        C = observation matrix
+        zeta = observation noise covariance matrix
         y_t = true measurement
         """
-        L_t = self.var_t @ C.T @ np.linalg.pinv(C @ self.var_t @ C.T + E)
-        self.mu_t = self.mu_t + L_t @ (y_t - C @ self.mu_t)
-        self.var_t = (np.eye(np.shape(L_t)[0]) - L_t @ C) @ self.var_t
+        sig_y = C @ self.sig_x @ C.T + zeta
+        L_t = np.linalg.solve(sig_y, self.sig_x @ C.T)
+        self.mu_x = self.mu_x + L_t @ (y_t.reshape((-1, 1)) - C @ self.mu_x)
+        self.sig_x = (np.eye(np.shape(L_t)[0]) - L_t @ C) @ self.sig_x
