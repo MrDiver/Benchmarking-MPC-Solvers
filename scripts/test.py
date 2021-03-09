bb72a72e60bb21e718fd574686a430fea700a3a0
MrDiver <mrdiverlp@gmail.com>
2021-03-10 00:11:32 +0100

    more plots

--- scripts/test.py
+++ scripts/test.py
@@ -37,23 +37,23 @@ experiment_path = "experiments/"+timestring
 
 env: Environment = PENV()
 model: Model = PEMOD()
-# env: Environment = CPSUENV()
-# model: Model = CPSUMOD()
-env: Environment = ACENV()
-model: Model = ACMOD()
+env: Environment = CPSUENV()
+model: Model = CPSUMOD()
+#env: Environment = ACENV()
+#model: Model = ACMOD()
 
 #model: Model = DummyModel(4, 1)
 # model = GEM(ENVIRONMENT)j
 
 
-params_cem = {"K": 100, "T": 30, "max_iter": 10,
+params_cem = {"K": 50, "T": 15, "max_iter": 5,
               "n_elite": 5, "epsilon": 1e-5, "alpha": 0.2, "instant_cost": (lambda x, u: 0), "std": 1}
 
-params_mppi = {"K": 100, "T": 30, "std": 1,
+params_mppi = {"K": 50, "T": 15, "std": 1,
                "terminal_cost": (lambda x: 0), "instant_cost": (lambda x, u: 0),
                "lam": 0.1}
 
-params_ilqr = {"T": 30, "max_iter": 10, "threshold": 1e-5}
+params_ilqr = {"T": 15, "max_iter": 5, "threshold": 1e-5}
 cem: CEM = CEM(model, params_cem)
 mppi: MPPI = MPPI(model, params_mppi)
 ilqr: ILQR = ILQR(model, params_ilqr)
