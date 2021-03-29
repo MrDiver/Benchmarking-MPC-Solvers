8b777a568f25af0df08c0486dfbc77ddc570f636
Tristan Schulz <mrdiverlp@gmail.com>
2021-03-29 16:34:44 +0200

    Delete old directory

--- old/controller/core.py
+++
@@ -1,9 +0,0 @@
-class Algorithm:
-    def __init__ (self):
-        pass
-
-    def step(self,t,x):
-        raise NotImplementedError
-
-    def print_state(self):
-        raise NotImplementedError
